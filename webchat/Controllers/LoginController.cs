2e4307c1a99d7d7708dea69e75d910174f754d21
77m3lsh7 7 <felopater.remon2020@gmail.com>
2025-01-08 23:42:03 +0200

    Initial commit

--- webchat/Controllers/LoginController.cs
+++ webchat/Controllers/LoginController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webchat.data;
 
 namespace webchat.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ChatDbcontect _chatDbcontect;
+        public LoginController(ChatDbcontect chatDbcontect)
+        {
+            _chatDbcontect = chatDbcontect;
+        }
         public IActionResult Index()
         {
             ViewData["HideNavbar"] = true;
