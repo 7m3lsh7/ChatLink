ea6c49a48c2173c669ecfa8bee0b184bbba7769d
77m3lsh7 7 <felopater.remon2020@gmail.com>
2025-01-10 20:17:16 +0200

    5

--- webchat/Controllers/LoginController.cs
+++ webchat/Controllers/LoginController.cs
@@ -50,3 +50,51 @@ namespace webchat.Controllers
                   
     }                      
 }
+
+/*
+ <nav class="navbar navbar-expand-lg color">
+        <div class="container-fluid">
+            <div class="navbar-column">
+                <div class="profile-picture">
+                <img src="https://via.placeholder.com/150" alt="Profile Picture">
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item pt-2 pb-2">
+                <a class="nav-link active" aria-current="page" href="./Index.html">
+                    <i class="fa-solid text-white fa-house"></i>
+                </a>
+                </li>
+                <li class="nav-item pt-2 pb-2">
+                <a class="nav-link" href="./chat.html">
+                    <i class="fa-solid text-white fa-bell"></i>
+                </a>
+                </li>
+                <li class="nav-item pt-2 pb-2">
+                <a class="nav-link" href="#">
+                    <i class="fa-solid text-white fa-message"></i>
+                </a>
+                </li>
+                <li class="nav-item pt-2 pb-2">
+                <a class="nav-link">
+                    <i class="fa-solid text-white fa-sliders"></i>
+                </a>
+                </li>
+            </ul>
+            </div>
+            <div class="logout">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#">
+                            <i class="fa-solid  text-white fa-right-from-bracket"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    </nav>
+ */
