7fd9b6b31f6a5afe032d1292e8d0e1a23111f9a0
77m3lsh7 7 <felopater.remon2020@gmail.com>
2025-01-26 21:45:42 +0200

    first finsh

--- webchat/Program.cs
+++ webchat/Program.cs
@@ -21,18 +21,20 @@ builder.Services.AddSession(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
