210ab9b7882b81d2f6f60993f11843116ea3be35
Abdulrahman Mahmoud <bm9412238@gmail.com>
2025-01-30 16:25:34 +0200

    Handle Error

---
+++ webchat/Controllers/ErrorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webchat.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        [Route("Error")]
+        public IActionResult General()
+        {
+            ViewData["HideNavbar"] = true;
+            ViewData["HideFooter"] = true;
+            Response.StatusCode = 500;
+            return View();
+        }
+
+        [Route("Error/404")]
+        public IActionResult NotFound()
+        {
+            ViewData["HideNavbar"] = true;
+            ViewData["HideFooter"] = true;
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        [Route("Error/500")]
+        public IActionResult ServerError()
+        {
+            ViewData["HideNavbar"] = true;
+            ViewData["HideFooter"] = true;
+            Response.StatusCode = 500;
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["HideNavbar"] = true;
+            ViewData["HideFooter"] = true;
+            return RedirectToAction("General");
+        }
+    }
+}
